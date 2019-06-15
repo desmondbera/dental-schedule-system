6f0b8198d2890cccfb07d817881d11d5df82ffa0
Desmond Beramendi <desmond.bera@gmail.com>
2019-06-15 18:43:11 -0400

    Initializing DentalScheduleSystem Project

---
+++ DentalScheduleSystem/src/ScheduleApp.java
@@ -0,0 +1,26 @@
+
+public class ScheduleApp {
+
+	public static void main(String[] args) {
+		System.out.println("Starting Dental Scheduling App...");
+		
+		//1. Once a patient lands on the page they will have 2 options
+			//1.1 - LOG IN
+			//1.2 - SIGN UP
+	
+		//2. If the user is already signed up (in our DB)
+		//		we will show the following options once they log in:
+		//	2.1 - Show appointments they made
+		//	2.2 - Show a list of hygienist's they can choose from OR search functionality
+		//	2.3 - Schedule an appointment 
+		// 	2.4 - Cancel an appointment
+		
+		//3. If the user is signing up we will show the following options:
+		//	3.1 - Enter name
+		//	3.2 - Enter email
+		//	3.3 - Enter Address
+		// 	3.4 - Preferred Hygienist Name
+		//	3.5 - Dental Office they frequent
+	}
+
+}
